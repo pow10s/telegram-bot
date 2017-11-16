05ad8c0091705fcd048f1043401431e3e0908514
Dima Stos <Dima Stos>
2017-11-16 02:00:24 +0200

    ci-added-start-command

--- bot.php
+++ bot.php
@@ -2,13 +2,11 @@
 require_once "vendor/autoload.php";
 
 try {
+    $bot2 = new \TelegramBot\Api\BotApi('');
     $bot = new \TelegramBot\Api\Client('438332110:AAFCgeVIz_vq6HJznmLqbvTcxbZ0v4lCEzY');
-    // or initialize with botan.io tracker api key
-    // $bot = new \TelegramBot\Api\Client('YOUR_BOT_API_TOKEN', 'YOUR_BOTAN_TRACKER_API_KEY');
 
-
-    $bot->command('hi', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), "Hello world" );
+    $bot->command('start', function ($message) use ($bot) {
+        $bot->sendMessage($message->getChat()->getId(), "Welocme ". $message->getChat()->getFirstName() . " to notification bot" );
     });
 
     $bot->run();
