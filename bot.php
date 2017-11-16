3e5e807697bf1b74ba652d583c3e5c7650f1f15d
Dima Stos <Dima Stos>
2017-11-16 21:11:08 +0200

    ci-help-command

--- bot.php
+++ bot.php
@@ -5,17 +5,7 @@ try {
     $bot = new \TelegramBot\Api\Client('438332110:AAFCgeVIz_vq6HJznmLqbvTcxbZ0v4lCEzY');
 
     $bot->command('start', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'Hello, ' .$message->getChat()->getFirstName() .'. thank`s for subscribing. Commands list: /help' );
-    });
-
-    $bot->command('help', function ($message) use ($bot){
-        $commandList = 'List of commands:
-                         /start - start work with bot
-                         /stop - stop work with bot
-                         /search - search posts by categories
-                         /admin - site administrator panel
-                         if you want get quote input random message to bot';
-        $bot->sendMessage($message->getChat()->getId, $commandList);
+        $bot->sendMessage($message->getChat()->getId(), 'Hello, ' .$message->getChat()->getFirstName() .'. Thank`s for subscribing. Commands list: /help' );
     });
 
     $bot->command('search', function ($message) use ($bot) {
@@ -29,6 +19,11 @@ try {
         );
         $bot->sendMessage($message->getChat()->getId(), "Search by:", null, false, null, $keyboard );
     });
+
+    $bot->command('help', function ($message) use ($bot) {
+        $bot->sendMessage($message->getChat()->getId(), 'Command list:');
+    });
+
 
     $bot->run();
 
