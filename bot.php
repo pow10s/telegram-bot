a5138b382e3c7e47f8bf990471b4e8bd03ef260a
Dima Stos <Dima Stos>
2017-11-16 20:50:02 +0200

    added help command

--- bot.php
+++ bot.php
@@ -5,7 +5,17 @@ try {
     $bot = new \TelegramBot\Api\Client('438332110:AAFCgeVIz_vq6HJznmLqbvTcxbZ0v4lCEzY');
 
     $bot->command('start', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), "Welcome, ". $message->getChat()->getFirstName() . ", to notification bot." );
+        $bot->sendMessage($message->getChat()->getId(), 'Hello, ' .$message->getChat()->getFirstName() .'. thank`s for subscribing. Commands list: /help' );
+    });
+
+    $bot->command('help', function ($message) use ($bot){
+        $commandList = 'List of commands:
+                         /start - start work with bot
+                         /stop - stop work with bot
+                         /search - search posts by categories
+                         /admin - site administrator panel
+                         <b>if you want get quote input random message</b>';
+        $bot->sendMessage($message->getChat()->getId, $commandList);
     });
 
     $bot->command('search', function ($message) use ($bot) {
@@ -19,9 +29,7 @@ try {
         );
         $bot->sendMessage($message->getChat()->getId(), "Search by:", null, false, null, $keyboard );
     });
-    $bot->callbackQuery(function (\TelegramBot\Api\Types\CallbackQuery $callbackQuery) use ($bot){
-        $callbackQuery->answerCallbackQuery($callbackQuery->getId(), 'test');
-    });
+
     $bot->run();
 
 } catch (\TelegramBot\Api\Exception $e) {
