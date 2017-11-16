59e862f51b1f333b9c82a20ba31a6afea8cb5f45
Dima Stos <Dima Stos>
2017-11-16 02:12:06 +0200

    ci-remake-search

--- bot.php
+++ bot.php
@@ -5,19 +5,13 @@ try {
     $bot2 = new \TelegramBot\Api\BotApi('');
     $bot2->
     $bot = new \TelegramBot\Api\Client('438332110:AAFCgeVIz_vq6HJznmLqbvTcxbZ0v4lCEzY');
+
     $bot->command('start', function ($message) use ($bot) {
         $bot->sendMessage($message->getChat()->getId(), "Welcome, ". $message->getChat()->getFirstName() . ", to notification bot." );
     });
 
     $bot->command('search', function ($message) use ($bot) {
-        $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
-            [
-                [
-                    ['text' => 'link', 'url' => 'https://core.telegram.org']
-                ]
-            ]
-        );
-        $bot->sendMessage($message->getChat()->getId(), "Search by:", null, false, null, $keyboard );
+        $bot->sendMessage($message->getChat()->getId(), "Search by:" );
     });
     $bot->run();
 
