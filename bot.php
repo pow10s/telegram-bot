b072db52abf0eb49b58d519cae193541e63f4b89
Dima Stos <Dima Stos>
2017-11-16 02:08:06 +0200

    ci-testing-api-keyboard

--- bot.php
+++ bot.php
@@ -3,12 +3,22 @@ require_once "vendor/autoload.php";
 
 try {
     $bot2 = new \TelegramBot\Api\BotApi('');
+    $bot2->
     $bot = new \TelegramBot\Api\Client('438332110:AAFCgeVIz_vq6HJznmLqbvTcxbZ0v4lCEzY');
-
     $bot->command('start', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), "Welocme ". $message->getChat()->getFirstName() . " to notification bot" );
+        $bot->sendMessage($message->getChat()->getId(), "Welcome, ". $message->getChat()->getFirstName() . ", to notification bot." );
     });
 
+    $bot->command('search', function ($message) use ($bot) {
+        $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+            [
+                [
+                    ['text' => 'link', 'url' => 'https://core.telegram.org']
+                ]
+            ]
+        );
+        $bot->sendMessage($message->getChat()->getId(), "Search by:", null, false, null, $keyboard );
+    });
     $bot->run();
 
 } catch (\TelegramBot\Api\Exception $e) {
