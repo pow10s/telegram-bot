be2e8d9654f954b00de39eeb6ee8dd1d9d948371
Dima Stos <Dima Stos>
2017-11-17 13:14:57 +0200

    ci-callbackquery

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ try {
         $bot->sendMessage($message->getChat()->getId(), 'Search by:',false, null, false, $keyboard);
     });
     $bot->callbackQuery(function (\TelegramBot\Api\Types\CallbackQuery $callbackQuery) use ($bot){
-        print_r($callbackQuery);
+        print_r($callbackQuery->getData());
 
     });
     $bot->run();
