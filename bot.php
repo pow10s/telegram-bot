5d93be74d7a340757bea80a4a0adca3341e994ca
stosdima <stosdima@gmail.com>
2017-11-18 13:05:35 +0200

    ci-added-other-cimmands

--- bot.php
+++ bot.php
@@ -2,10 +2,11 @@
 require_once "vendor/autoload.php";
 try {
     $bot = new \TelegramBot\Api\Client('438332110:AAFCgeVIz_vq6HJznmLqbvTcxbZ0v4lCEzY');
-    $bot->command('start', function ($message) use ($bot){
-        $bot->sendMessage($message->getChat()->getId(), 'Hello,' .$message->getChat()->getFirstName().', thank`s for subscribing. Commands list: /help');
+    $bot->command('start', function ($message) use ($bot) {
+        $bot->sendMessage($message->getChat()->getId(),
+            'Hello,' . $message->getChat()->getFirstName() . ', thank`s for subscribing. Commands list: /help');
     });
-    $bot->command('help', function ($message) use ($bot){
+    $bot->command('help', function ($message) use ($bot) {
         $commandList = 'List of commands:
                          /start - start work with bot
                          /stop - stop work with bot
@@ -14,7 +15,7 @@ try {
                          if you want get quote input random message';
         $bot->sendMessage($message->getChat()->getId(), $commandList);
     });
-    $bot->command('search', function ($message) use ($bot){
+    $bot->command('search', function ($message) use ($bot) {
         $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
             [
                 [
@@ -23,32 +24,40 @@ try {
                 ]
             ]
         );
-        $bot->sendMessage($message->getChat()->getId(), 'Search by:',null, false, null, $keyboard);
+        $bot->sendMessage($message->getChat()->getId(), 'Search by:', null, false, null, $keyboard);
     });
-    $bot->command('admin', function($message) use ($bot){
+    $bot->command('admin', function ($message) use ($bot) {
         /*Need verification if logged user*/
         $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
             [
                 [
                     ['text' => 'Login', 'callback_data' => 'login'],
+                    ['text' => 'Create post', 'callback_data' => 'create-post'],
+                    ['text' => 'Delete post', 'callback_data' => 'delete-post'],
                 ]
             ]
         );
         /*End verification if logged user*/
-        $bot->sendMessage($message->getChat()->getId(), 'Hello, you are in admin panel!', null, false ,null, $keyboard);
+        $bot->sendMessage($message->getChat()->getId(), 'Hello, you are in admin panel!', null, false, null, $keyboard);
 
     });
 
-    $bot->callbackQuery(function (\TelegramBot\Api\Types\CallbackQuery $callbackQuery) use ($bot){
-        if($callbackQuery->getData() == 'categories'){
+    $bot->callbackQuery(function (\TelegramBot\Api\Types\CallbackQuery $callbackQuery) use ($bot) {
+        if ($callbackQuery->getData() == 'categories') {
             /*WORDPRESS CODE...*/
             $bot->sendMessage($callbackQuery->getFrom()->getId(), 'List of categories:');
-        }elseif($callbackQuery->getData() == 'keyword'){
+        } elseif ($callbackQuery->getData() == 'keyword') {
             /*WORDPRESS CODE...*/
             $bot->sendMessage($callbackQuery->getFrom()->getId(), 'Searching by keywords');
-        }elseif ($callbackQuery->getData() == 'login'){
+        } elseif ($callbackQuery->getData() == 'login') {
             /*WORDPRESS CODE...*/
             $bot->sendMessage($callbackQuery->getFrom()->getId(), 'Login functionality');
+        } elseif ($callbackQuery->getData() == 'create-post') {
+            /*WORDPRESS CODE...*/
+            $bot->sendMessage($callbackQuery->getFrom()->getId(), 'Post created');
+        } elseif ($callbackQuery->getData() == 'delete-post') {
+            /*WORDPRESS CODE...*/
+            $bot->sendMessage($callbackQuery->getFrom()->getId(), 'Post deleted');
         }
     });
     $bot->run();
