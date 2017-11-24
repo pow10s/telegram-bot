81be0680459a27007a138d83f12f8aa521d55a35
stosdima <stosdima@gmail.com>
2017-11-24 20:32:10 +0200

    ci-added-message

--- bot.php
+++ bot.php
@@ -8,7 +8,8 @@ try {
             str_replace("<br/>", "\n", json_decode('"' . $result[1][0] . '"')));
     });
     $bot->command('qaanswer', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), json_encode(file_get_contents('php://input')));
+        $data = json_encode(file_get_contents('php://input'))
+        $bot->sendMessage($message->getChat()->getId(), $data['message']['text']);
     });
     $bot->run();
 } catch (\TelegramBot\Api\Exception $e) {
